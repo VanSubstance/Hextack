fd973e13f643219f2f6ba244b89ba584a9da250e
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2023-07-01 14:53:18 +0900

    Add: Basic Varietion of Units
    #3
    
    + 기물 기본 종류 추가

--- Assets/Scripts/Common/GlobalDictionary.cs
+++ Assets/Scripts/Common/GlobalDictionary.cs
@@ -1,10 +1,6 @@
-﻿using System;
+﻿using Assets.Scripts.Unit;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
-using Assets.Scripts.Unit;
 
 public static class GlobalDictionary
 {
